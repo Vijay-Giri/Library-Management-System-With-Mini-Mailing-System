e16cad23b8228b0a08445a59beda7596b297e37a
Vijay-Giri <lit2016038@iiitl.ac.in>
2019-02-16 14:45:21 +0530

    First commit

---
+++ src/javaconnect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn;
+     public static Connection ConnecrDb()
+     {
+         try
+         {
+             Class.forName("org.sqlite.JDBC");
+             Connection conn= DriverManager.getConnection("jdbc:sqlite:C:\\Users\\ASUS\\Documents\\NetBeansProjects\\dbms_project\\dbms.sqlite");
+             return conn;
+         }
+         catch(Exception e)
+         {
+             JOptionPane.showMessageDialog(null, e);
+             return null;
+         }
+     }
+    
+}
